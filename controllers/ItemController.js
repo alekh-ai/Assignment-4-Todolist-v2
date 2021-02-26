11a90729d591a6e5df39aaecebe351f46b308540
alekh-ai <78842369+alekh-ai@users.noreply.github.com>
2021-02-26 15:59:37 +0530

    Controller and Routes Fixed

--- controllers/ItemController.js
+++ controllers/ItemController.js
@@ -3,9 +3,11 @@ const Item = require('../models/ItemModel');
 const ItemController = () => {};
 
 // Create Item
-ItemController.createItem = (data) => {
+ItemController.createItem = (req, res) => {
+	const newItem = req.body.newItem;
+
 	const item = new Item({
-		name: data,
+		name: newItem,
 	});
 	item.save((err, doc) => {
 		if (err) {
@@ -14,17 +16,41 @@ ItemController.createItem = (data) => {
 			console.log('Item Saved: ', doc);
 		}
 	});
+
+	res.redirect('/');
 };
 
 // Find and Remove by Id
-ItemController.removeItemById = (itemId) => {
-	Item.findByIdAndRemove(itemId, (err) => {
+ItemController.removeItemById = (req, res) => {
+	const checkedItemId = req.body.checked;
+
+	Item.findByIdAndRemove(checkedItemId, (err) => {
 		if (err) {
 			console.log(err);
 		} else {
-			console.log('Item Removed Successfully ', itemId);
+			console.log('Item Removed Successfully ', checkedItemId);
 		}
 	});
+
+	res.redirect('/');
+};
+
+// Find items
+ItemController.findItems = (req, res) => {
+	Item.find((err, foundItems) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.render('list', { listTitle: 'Today', newListItem: foundItems });
+		}
+	});
+};
+
+// New list items
+ItemController.newList = (req, res) => {
+	const customList = req.params.customURL;
+
+	res.send(`<h1 align=center> ${customList} </h1>`);
 };
 
 module.exports = ItemController;
--- routes/appRoute.js
+++ routes/appRoute.js
@@ -1,41 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const Item = require('../models/ItemModel');
+// const Item = require('../models/ItemModel');
 const ItemController = require('../controllers/ItemController');
 
 // -------------- Get all current List Items
-router.get('/', (req, res) => {
-	Item.find((err, foundItems) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.render('list', { listTitle: 'Today', newListItem: foundItems });
-		}
-	});
-});
+router.get('/', ItemController.findItems);
 
 // -------------- Create a new List Item
-router.post('/', (req, res) => {
-	const newItem = req.body.newItem;
-
-	ItemController.createItem(newItem);
-	res.redirect('/');
-});
+router.post('/', ItemController.createItem);
 
 // -------------- Delete Checked  List Item
-router.post('/delete', (req, res) => {
-	const checkedItemId = req.body.checked;
-
-	ItemController.removeItemById(checkedItemId);
-	res.redirect('/');
-});
+router.post('/delete', ItemController.removeItemById);
 
 // -------------- Create a new List
-router.get('/:customURL', (req, res) => {
-	const customList = req.params.customURL;
-
-	res.send(`<h1 align=center> ${customList} </h1>`);
-});
+router.get('/:customURL', ItemController.newList);
 
 module.exports = router;
